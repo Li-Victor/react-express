220870ee642e00fad2929924f98fefe6971e6c23
Victor Li <victorli5674@yahoo.com>
2017-08-19 01:59:12 -0700

    just having one package.json file

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 var port = process.env.PORT || 3000;
